cc83a1867bd363e0e2947ce205f35b6407c1a118
Tanmay4269 <tanmay4269@gmail.com>
2023-01-30 14:46:28 +0530

    made a more efficient code to make the csv file

--- colartz_conj_visual/colartz_conj.cpp
+++ colartz_conj_visual/colartz_conj.cpp
@@ -3,6 +3,8 @@
 #include <unordered_map>
 using namespace std;
 
+int my_size = 100'000;
+
 unordered_map<int, int> memo;
 
 int collatz(int n) {
@@ -18,16 +20,12 @@ int collatz(int n) {
 }
 
 void foo() {
-    // Open the file in write mode
     ofstream file("coords.csv");
     
     file << "x,y\n";
 
-    for (int i = 2; i < 200'000; i++) {
-        int count = collatz(i);
-
-        file << i << "," << count << "\n";
-    }
+    for (int i = 2; i < my_size; i++)
+        file << i << "," << collatz(i) << "\n";
 
     file.close();
 }
